549af95d95ffc1f9a61de03762ca3b5d628241f0
Willem van Straten <vanstraten.willem@gmail.com>
2022-07-17 20:37:48 +1200

    merge errors fixed up

--- More/Applications/pcm.C
+++ More/Applications/pcm.C
@@ -1791,9 +1791,6 @@ SystemCalibrator* pcm::measurement_equation_modeling (const string& binfile,
   if (flux_cal)
     model->set_flux_calibrator (flux_cal);
   
-  cerr << "pcm: set calibrators" << endl;
-  model->set_calibrators (calibrator_filenames);
-  model->set_calibrator_preprocessor (standard_options);
   // archive from which pulse phase bins will be chosen
   Reference::To<Pulsar::Archive> autobin;
 
@@ -1828,7 +1825,9 @@ SystemCalibrator* pcm::measurement_equation_modeling (const string& binfile,
     cerr << "pcm: loading calibrator filenames from " << calfile << endl;
     stringfload (&filenames, calfile);
   }
-  
+
+  cerr << "pcm: set calibrators" << endl;
+  model->set_calibrator_preprocessor (standard_options);
   model->set_calibrators (filenames);
 
   if (phmin != phmax)
