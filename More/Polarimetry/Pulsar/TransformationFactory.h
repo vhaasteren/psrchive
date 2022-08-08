a7d08e4a2c9e42ce2738693ffe399b484f72cec6
Willem van Straten <vanstraten.willem@gmail.com>
2022-08-08 14:48:22 +1200

    factory constructs children of MEAL::Complex2

---
+++ More/Polarimetry/Pulsar/TransformationFactory.h
@@ -0,0 +1,28 @@
+//-*-C++-*-
+/***************************************************************************
+ *
+ *   Copyright (C) 2022 by Willem van Straten
+ *   Licensed under the Academic Free License version 2.1
+ *
+ ***************************************************************************/
+
+#ifndef __Calibration_TransformationFactory_h
+#define __Calibration_TransformationFactory_h
+
+#include "MEAL/Complex2.h"
+
+namespace Calibration {
+
+  //! Constructs and configures new Complex2 given descriptive string
+  class TransformationFactory 
+  {
+  public:
+
+    TransformationFactory ();
+
+    //! Construct a new transformation
+    virtual MEAL::Complex2* new_transformation (const std::string&);
+  };
+}
+
+#endif
