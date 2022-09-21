28dea638c47625be536984edb36b95a946231fc9
Willem van Straten <vanstraten.willem@gmail.com>
2022-09-21 12:00:18 +1200

    repeat test of 273x273 matrix inverse 1000 times

--- More/MEAL/test_GaussJordan.C
+++ More/MEAL/test_GaussJordan.C
@@ -11,38 +11,57 @@
 
 using namespace std;
 
-int main ()
+int once ()
 {
-  vector< vector<double> > test (13, vector<double>(13) );
+  unsigned dim = 273;
+
+  vector< vector<double> > test (dim, vector<double>(dim) );
   random_matrix (test, 10.0);
 
-  vector< vector<double> > identity (13, vector<double>(13) );
-  for (unsigned i=0; i<13; i++)
-    for (unsigned j=0; j<13; j++)
-      if (i==j)
-        identity[i][j] = 1.0;
-      else
-        identity[i][j] = 0.0;
+  vector< vector<double> > copy = test;
 
+  vector< vector<double> > other (dim, vector<double>(1) );
+  random_matrix (other, 10.0);
 
-  MEAL::GaussJordan (test, identity);
+  MEAL::GaussJordan (test, other);
 
-  for (unsigned i=0; i<13; i++)
+  double tolerance = 2e-12;
+
+  for (unsigned irow=0; irow < dim; irow++)
+    for (unsigned icol=0; icol < dim; icol++)
+    {
+      double sum = 0.0;
+      for (unsigned k=0; k < dim; k++)
+        sum += test[irow][k] * copy[k][icol];
+
+      double expect = (irow == icol) ? 1.0 : 0.0;
+      if ( fabs(sum - expect) > tolerance)
+      {
+        cerr << "irow=" << irow << " icol=" << icol << " sum=" << sum << endl;
+        return -1;
+      }
+    }
+
+  return 0;
+}
+
+int main ()
+{
+  unsigned ntrial = 1000;
+  unsigned nerr = 0;
+
+  for (unsigned i=0; i < ntrial; i++) try
   {
-    for (unsigned j=0; j<13; j++)
-      cerr << test[i][j] << " ";
-    cerr << endl;
+    if (once () != 0)
+      nerr ++;
+  }
+  catch (Error& error)
+  {
+    cerr << error << endl;
+    nerr ++;
   }
 
-  cerr << endl << endl;
-
-  for (unsigned i=0; i<13; i++)
-  {
-    for (unsigned j=0; j<13; j++)
-      cerr << identity[i][j] << " ";
-    cerr << endl;
-  }
-
+  cerr << nerr << " errors in " << ntrial << " trials" << endl;
   return 0;
 }
 
