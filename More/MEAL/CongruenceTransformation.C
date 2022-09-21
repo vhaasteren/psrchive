90e070abb66057a05fedb895ddcf0b81603555f3
Willem van Straten <vanstraten.willem@gmail.com>
2022-09-21 22:35:01 +1200

    more error checking (inputs and outputs)

--- More/MEAL/CongruenceTransformation.C
+++ More/MEAL/CongruenceTransformation.C
@@ -76,6 +76,36 @@ MEAL::CongruenceTransformation::calculate (Jones<double>& result,
   // compute input and partial derivatives with respect to input parameters
   Jones<double> input_jones = input->evaluate (input_grad_ptr);
 
+  if (!isfinite (xform_jones))
+  {
+#if _DEBUG
+    cerr << "MEAL::CongruenceTransformation::calculate non-finite transformation" << endl;
+    string state;
+    transformation->print(state);
+    cerr << state;
+#endif
+
+    Error error (InvalidState, "MEAL::CongruenceTransformation::calculate",
+                 "non-finite transformation=");
+    error << xform_jones;
+    throw error;
+  }
+
+  if (!isfinite (input_jones))
+  {
+#if _DEBUG
+    cerr << "MEAL::CongruenceTransformation::calculate non-finite input" << endl;
+    string state;
+    input->print(state);
+    cerr << state;
+#endif
+
+    Error error (InvalidState, "MEAL::CongruenceTransformation::calculate",
+                 "non-finite input=");
+    error << input_jones;
+    throw error;
+  }
+
   // set the result
   result = xform_jones * input_jones * xform_herm;
 
